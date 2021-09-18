9946aa3c415c02cf2e7a7f3a570e97504f33eea5
Hannah Ajayi <hannahajayi@Hannahs-MacBook-Pro.local>
2021-09-18 14:52:02 -0400

    changes made to form

--- src/Components/ExpenseContainer.js
+++ src/Components/ExpenseContainer.js
@@ -9,7 +9,15 @@ const ExpenseContainer = ({expenses, expense, date, cost, handleExpenseInput, ha
         return(
             <div>
                 <div id="form">
-                   <form onSubmit={create_expense}>
+                    <form className="income-form" onSubmit={create_expense}>
+                        <div className="form-inner">
+                            <input type="text" placeholder="Expense" value={expense} onChange={handleExpenseInput} className="input"/> 
+                            <input type="text"  placeholder="Cost" value={cost} onChange={handleDateInput} className="input"/>
+                            <input type="text" placeholder="Date" value={date} onChange={handleCostInput} className="input"/>
+                            <input type="submit" value="Add Expense" className="submit"/>
+                        </div>
+                    </form>
+                   {/* <form onSubmit={create_expense}>
                         <input 
                         className="input1"
                         placeholder="Expense"
@@ -37,25 +45,25 @@ const ExpenseContainer = ({expenses, expense, date, cost, handleExpenseInput, ha
                         style={{color: "#C0C0C0"}}
                         />
                         <button class="submit">Add Expense</button>
-                    </form>
+                    </form> */}
                     </div>
-            
-         
+                
+                    <div className="expense_list">
                         <ul>
-                            {
-                                expenses.map(expense => {
-                                    return(
-                                        <Expense key={expense.id} 
-                                        handleExpenseList={handleExpenseList} 
-                                        expense_description={expense.expense_description} 
-                                        date={expense.date}
-                                        cost={expense.cost}
-                                        />
-                                    )
-                                })
-                            }
-                        </ul>
-
+                                {
+                                    expenses.map(expense => {
+                                        return(
+                                            <Expense key={expense.id} 
+                                            handleExpenseList={handleExpenseList} 
+                                            expense_description={expense.expense_description} 
+                                            date={expense.date}
+                                            cost={expense.cost}
+                                            />
+                                        )
+                                    })
+                                }
+                            </ul>
+                    </div>
             </div>    
 
         );
